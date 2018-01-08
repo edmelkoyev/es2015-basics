cb280233f222b74d529a22faa0eeb0fadefae8b4
Edward Melkoyev <edmelkoyev@gmail.com>
2018-01-08 23:37:51 +0300

    Classes:  setters and getters

--- src/classes.js
+++ src/classes.js
@@ -1,8 +1,20 @@
 class Task {
     constructor(title = Task.getDefaultTitle()) {
         this.title = title;
-        this.done = false;
+        this._done = false;
         Task.count += 1;
+    }
+
+    get done() {
+        return this._done === true ? 'Completed' : 'Not Completed';
+    }
+
+    set done(val){
+        if (val !== undefined && typeof val === 'boolean'){
+            this._done = val;
+        } else {
+            console.error('Error: value must be true/false');
+        }
     }
 
     complete() {
@@ -19,17 +31,10 @@ class Task {
 Task.count = 0;
 
 let task = new Task('Clean-up the room');
-let task2 = new Task('Go to the school');
-let task3 = new Task();
-
-console.log(typeof Task);
-console.log(typeof task);
-console.log(task instanceof Task);
-
-console.log(task.title);
-console.log(task2.title);
-console.log(task3.title);
-
 console.log('Task Count:', Task.count);
 
+console.log(task.done, task._done);
+
 task.complete();
+
+console.log(task.done, task._done);
